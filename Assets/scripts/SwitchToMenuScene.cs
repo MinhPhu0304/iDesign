bb3bf40fd405b04aee29a6dc1388c064cec18221
Minh-Phu <Lminhphu0304@gmail.com>
2019-08-11 12:44:14 +1200

    Add script to navigate from log in to Menu scene

---
+++ Assets/scripts/SwitchToMenuScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SwitchToMenuScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchToMenu()
+    { 
+        SceneManager.LoadScene("Menu");
+    }
+}
