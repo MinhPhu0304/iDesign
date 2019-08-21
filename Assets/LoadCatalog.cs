408d958d4df26e09c619e3b383f78de7106f46e7
Minh-Phu <Lminhphu0304@gmail.com>
2019-08-22 09:59:20 +1200

    UI scripting add button

---
+++ Assets/LoadCatalog.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+//using UnityEngine.Experimental.UIElements;
+
+public class LoadCatalog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject content = GameObject.Find("Content");
+        GameObject button = new GameObject();
+
+        button.AddComponent<CanvasRenderer>();
+        button.AddComponent<RectTransform>();
+        Button mButton = button.AddComponent<Button>();
+        Image mImage = button.AddComponent<Image>();
+        mButton.targetGraphic = mImage;
+
+        button.transform.position = new Vector3(0, 0, 0);
+        button.transform.SetParent(content.transform);
+        button.GetComponent<Button>().onClick.AddListener(Testmethod);
+
+    }
+
+    void Testmethod()
+    {
+        Debug.Log("Button Pressed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
