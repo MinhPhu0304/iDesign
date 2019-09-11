ad8ccf053d10f793912b975cf9eb18f6761226d6
KadinHoneyfield <kadinhoneyfield@gmail.com>
2019-09-11 14:10:16 +1200

    Modified CatalogUI: made thumbnails for items, added preview and info button to listings

--- Assets/scripts/LoadCatalog.cs
+++ Assets/scripts/LoadCatalog.cs
@@ -15,15 +15,20 @@ public class LoadCatalog : MonoBehaviour
 
     [SerializeField] GameObject buttonPrefab;
 
+    [SerializeField] GameObject itemListingPrefab;
+
     public ArrayList loadedItems = new ArrayList();
 
-    ARSceneController sceneController = new ARSceneController();
+    GameObject sceneController;
 
+    /*ARSceneController sceneController;*/
 
     // Start is called before the first frame update
     void Start()
     {
         generateItems();
+        sceneController = new GameObject();
+        sceneController.AddComponent<ARSceneController>();
 
         GameObject content = GameObject.Find("Content");
         //resolutions = Screen.resolutions;
@@ -38,7 +43,7 @@ public class LoadCatalog : MonoBehaviour
             button.transform.position = position;
         }*/
 
-        int itemNo = 0;
+        /*int itemNo = 0;
         foreach (Item itemInList in loadedItems)
         {
             GameObject itemButton = Instantiate(buttonPrefab);
@@ -50,13 +55,35 @@ public class LoadCatalog : MonoBehaviour
             itemButton.transform.SetParent(content.transform);
 
             //Create position
-            Vector3 position = new Vector3(0, itemNo * -30-10);
+            Vector3 position = new Vector3(0, itemNo * -30-30);
             //Set position to button
             itemButton.transform.position = position;
 
             itemNo++;
-            
+        }*/
 
+        int listingNo = 0;
+
+        foreach (Item itemInList in loadedItems)
+        {
+            GameObject itemListing = Instantiate(itemListingPrefab);
+            itemListing.GetComponentInChildren<Text>().text = itemInList.GetName(); //Set Titletext
+            itemListing.name = $"Listing: {itemInList.GetItemID()} {itemInList.GetName()}"; //Set name of gameobject
+
+            Sprite thumbnailSprite = Resources.Load<Sprite>($"Thumbnails/{ itemInList.GetName()}") as Sprite;
+            itemListing.transform.Find("Thumbnail").GetComponent<Image>().sprite = thumbnailSprite; //Set thumbnail
+
+            itemListing.transform.Find("Preview Button").GetComponent<Button>().onClick.AddListener(() => NavigateToScene(itemInList.GetName())); //Make previewbutton go to ARScene
+            //itemListing.transform.Find("Info Button").GetComponent<Button>().onClick.AddListener(() => NavigateToInfoScene(itemInList.GetName())); //Make info button go to info scene
+
+            itemListing.transform.SetParent(content.transform); //Set listing parent
+            RectTransform rt = itemListing.GetComponent<RectTransform>(); //get size of listing
+            rt.sizeDelta = new Vector2(440, 125); //set size of listing
+
+            Vector3 position = new Vector3(0, listingNo * -135-75); //Create position
+            itemListing.transform.position = position; //Set position to listing
+
+            listingNo++;
         }
     }
 
@@ -64,10 +91,9 @@ public class LoadCatalog : MonoBehaviour
     {
         Debug.Log($"Loading resource: {name}");
         GameObject selectedObject = Resources.Load($"Models/{name}") as GameObject;
-        sceneController.ChangeObjectToPlace(selectedObject);
+        sceneController.GetComponent<ARSceneController>().ChangeObjectToPlace(selectedObject);
 
-        SceneManager.LoadScene("ARScene");
-        
+        SceneManager.LoadScene("ARScene");      
     }
 
 
