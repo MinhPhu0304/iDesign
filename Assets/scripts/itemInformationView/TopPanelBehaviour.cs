35eef0560d31839e994486d31e637e4ee81f4834
Benedict Velasco <zvx8490@autuni.ac.nz>
2019-09-12 12:51:46 +1200

    Renaming Scripts
    Renaming scripts to more approiate names

---
+++ Assets/scripts/itemInformationView/TopPanelBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TopPanel : MonoBehaviour
+{
+    public GameObject TitleItemNameText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text name = TitleItemNameText.GetComponent<Text>();
+        Item currentItem = ItemDisplayPanel.currentItem;
+        if (currentItem != null)
+            name.text = ItemDisplayPanel.currentItem.GetName();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene("Catalog");
+    }
+}
