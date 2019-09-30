0c4cca77bd9af98bc8005fefa3dd5868454fc5dd
benNthen <53241776+benNthen@users.noreply.github.com>
2019-09-30 22:11:58 +1300

    Filter Panel
    +Filter Button next to search bar can be toggled to make panel appear
    +Filter panel with "Back" button functionality

---
+++ Assets/scripts/FilterOpen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilterOpen : MonoBehaviour
+{
+    public GameObject filterPanel;
+
+    public void OpenPanel()
+    {
+        if(filterPanel != null)
+        {
+            bool isActive = filterPanel.activeSelf;
+
+            filterPanel.SetActive(!isActive);
+        }
+    }
+}
