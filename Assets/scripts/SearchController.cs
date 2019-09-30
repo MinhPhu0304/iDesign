e7640beaa7a3c616263e81873ab4471281d89096
benNthen <53241776+benNthen@users.noreply.github.com>
2019-09-30 18:04:58 +1300

    Added filter feature

---
+++ Assets/scripts/SearchController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SearchController : MonoBehaviour
+{
+    private int num;
+
+    //private int countGuess;
+
+    [SerializeField]
+    private InputField input;
+
+    [SerializeField]
+    private Text text;
+
+
+    void Awake()
+    {
+        num = Random.Range(0, 100);
+        text.text = "Guess A Number Between 0 and 100";
+       // input = GameObject.Find("InputField").GetComponent<InputField>();
+    }
+
+    public void GetInput(string guess)
+    {
+        //Debug.Log("You entered " + guess);
+        ComparedGuesses(int.Parse(guess));
+        input.text = "";
+    }
+
+    void ComparedGuesses(int guess)
+    {
+        if (guess == num)
+        {
+            text.text = "You Guessed Correctly The Number Was " + guess;
+        }
+        else if(guess<num)
+        {
+            text.text = "Your Guess Number is Less Than The Number You Are Trying To Guess";
+        }
+        else if(guess> num)
+        {
+            text.text = "Your Guess Is Greater Than The Number You Are Trying To Guess";
+        }
+    }
+}
