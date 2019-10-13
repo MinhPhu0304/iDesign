c06c459cf5f790c312e259842637e41ced2abac3
MillanUka <41311473+MillanUka@users.noreply.github.com>
2019-10-14 09:09:07 +1300

    Addd more to the PopularItem script
    Made some additions to the PopularItem script. However there are still some problem with getting the UI to show up.

--- Assets/scripts/PopularItemsBehaviour.cs
+++ Assets/scripts/PopularItemsBehaviour.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
+
 public class PopularItemsBehaviour : MonoBehaviour
 {
+    public GameObject popularItemPrefab;
     public List<Item> itemList;
     public GameObject popularItemPanel;
+    public List<GameObject> itemListings;
     //Setting the number of popular items to display to 10
-    public int numberOfPopularItems = 10;
+    public int MAX_NUMBER_OF_POPULAR_ITENS = 10;
     // Start is called before the first frame update
     void Start()
     {
         itemList = new List<Item>();
+        itemListings = new List<GameObject>();
+
+        queryItems();
+        updatePopularItems();
     }
 
     // Update is called once per frame
@@ -43,17 +51,74 @@ public class PopularItemsBehaviour : MonoBehaviour
     {
         itemList.Sort();
 
-        //Checking whether there is less items than the set number of popular items
-        //IF there are then set the number of popular item to display to number of the list
-        if(itemList.Count < numberOfPopularItems)
+        int numberOfPopularItems;
+
+        // Check if the item list is less than the max number of popular items
+        // If it is greater than the max number of items make it eual the max number
+        if (itemList.Count < MAX_NUMBER_OF_POPULAR_ITENS)
         {
             numberOfPopularItems = itemList.Count;
+        } else
+        {
+            numberOfPopularItems = MAX_NUMBER_OF_POPULAR_ITENS;
         }
 
         GameObject content = GameObject.Find("Content");
 
-        }
+        for (int i =0; i < numberOfPopularItems; ++i)
+        {
+            GameObject itemListing = Instantiate(popularItemPrefab);
+            Item itemInList = itemList[i];
 
+            itemListing.SetActive(false); //Not shown when first loaded
 
+            itemListing.GetComponentInChildren<Text>().text = itemInList.GetName(); //Set Titletext
+            itemListing.GetComponentInChildren<Text>().fontSize = 30;
+            itemListing.name = $"Listing: {itemInList.GetItemID()} {itemInList.GetName()}"; //Set name of gameobject
+
+            Sprite thumbnailSprite = Resources.Load<Sprite>($"Thumbnails/{ itemInList.GetName()}") as Sprite;
+            itemListing.transform.Find("Thumbnail").GetComponent<Image>().sprite = thumbnailSprite; //Set thumbnail
+
+            itemListing.transform.Find("Preview Button").GetComponent<Button>().onClick.AddListener(() => NavigateToARScene(itemInList.GetName())); //Make previewbutton go to ARScene
+            itemListing.transform.Find("Info Button").GetComponent<Button>().onClick.AddListener(() => NavigateToInfoScene(itemInList)); //Make info button go to info scene
+
+            itemListing.transform.SetParent(content.transform, false); //Set listing parent
+
+            //set size of listing
+            GameObject scrollView = GameObject.Find("Scroll View");
+            RectTransform rt = itemListing.GetComponent<RectTransform>();
+            rt.sizeDelta = new Vector2(scrollView.GetComponent<RectTransform>().rect.width - 80, 150);
+
+            itemListings.Add(itemListing);
+            
+            showListing(itemInList);
+            itemListing.SetActive(true);
+
+        }    
+    }
+
+    private void showListing(Item listing)
+    {
+        Debug.Log($"Looking for: Listing: {listing.GetItemID()} {listing.GetName()}");
+
+        GameObject content = GameObject.Find("Content");
+        GameObject toShow = content.transform.Find($"Listing: { listing.GetItemID()} { listing.GetName()}").gameObject;
+
+        toShow.SetActive(true);
+    }
+
+    private void NavigateToInfoScene(Item itemToShow)
+    {
+        Debug.Log("Navigate to infoscene");
+        //itemSceneController.GetComponent<ItemDisplayPanelBehaviour>().SetCurrentItem(itemToShow);
+    }
+
+    private void NavigateToARScene(string name)
+    {
+        Debug.Log($"Loading resource: {name}");
+        GameObject selectedObject = Resources.Load($"Models/{name}") as GameObject;
+        //sceneController.GetComponent<ARSceneController>().ChangeObjectToPlace(selectedObject);
+
+        //SceneManager.LoadScene("ARScene");
     }
 }
