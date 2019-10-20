901a2859b57d7629c1e8dd5f63eaefd4d84ef302
KadinHoneyfield <kadinhoneyfield@gmail.com>
2019-10-20 23:09:25 +1300

    Added item buttons to manipulation scene. Removed Andy Object.

---
+++ Assets/scripts/ARScripts/ItemPanelBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemPanelBehaviour : MonoBehaviour
+{
+    private ItemManager itemManager;
+    public GameObject ItemImagePrefab;
+    public GameObject Panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemManager = GameObject.Find("Item Manager").GetComponent<ItemManager>();
+        GenerateItemButtons();
+    }
+
+    public void GenerateItemButtons()
+    {
+        foreach (GameObject model in itemManager.selectableModels)
+        {
+            GameObject modelButton = Instantiate(ItemImagePrefab);
+
+            modelButton.transform.SetParent(Panel.transform, false);
+
+            Sprite thumbnailSprite = Resources.Load<Sprite>($"Thumbnails/{model.name}") as Sprite;
+            modelButton.GetComponent<Image>().sprite = thumbnailSprite;
+
+            modelButton.GetComponent<Button>().onClick.AddListener(() => ChangePlacedItem(model));
+        }
+    }
+
+    public void ChangePlacedItem(GameObject model)
+    {
+        itemManager.ObjectToPlace = model;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
